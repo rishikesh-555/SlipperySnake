01ea3dc5300c937d7ca3446544a66ba9331dbd10
rishithecoder <72120688+rishithecoder@users.noreply.github.com>
2022-06-08 12:25:57 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// Game Constants
+let direction={x:0,y:0};
+let foodSound=new Audio('food.mp3');
+let gameOverSound=new Audio('gameovermp3');
+let moveSound=new Audio('move.mp3');
+let musicSound= new Audio('music.mp3');
+
+
+//Game Functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Main Logic Starts Here
+window.requestAnimationFrame(main);
